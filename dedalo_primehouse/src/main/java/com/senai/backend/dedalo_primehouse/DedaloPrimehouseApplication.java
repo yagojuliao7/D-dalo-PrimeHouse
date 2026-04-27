99c2e6320a52c46decd18608b87ab06c150b76ae
yagojuliao7 <yago.juliao@aluno.senai.br>
2026-04-27 14:14:07 -0300

    Dedalo

---
+++ dedalo_primehouse/src/main/java/com/senai/backend/dedalo_primehouse/DedaloPrimehouseApplication.java
@@ -0,0 +1,14 @@
+package com.senai.backend.dedalo_primehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DedaloPrimehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DedaloPrimehouseApplication.class, args);
+		System.out.println("Saracuruna!!!!!!!!");
+	}
+
+}
---
+++ dedalo_primehouse/src/test/java/com/senai/backend/dedalo_primehouse/DedaloPrimehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.backend.dedalo_primehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DedaloPrimehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
